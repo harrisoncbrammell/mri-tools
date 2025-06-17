9a1dde33026425bae6bcee25b3b411b6fb7918d4
harrisoncbrammell <harrisoncbrammell@gmail.com>
2025-06-17 18:48:33 -0500

    fixed initial bugs

--- dicomto4d.m
+++ dicomto4d.m
@@ -1,15 +1,15 @@
 function data = dicomto4d(path)
    %create list of 3d dicom files ordering based on name (expects 1.dcm,
    %2.dcm, etc as produced by Siemens MRI scanners in the Auburn MRI Lab
-   files = natsort(dir(fullfile(path, '*.dcm')).names);
+   files = natsort({dir(fullfile(path, '*.dcm')).name});
    numFiles = length(files);
 
    %create 4d data array based on size of a first 3d dicom array
-   [x, y, z] = size(double(squeeze(dicomread(fullfile(path, files(1).name)))));
+   [x, y, z] = size(double(squeeze(dicomread(fullfile(path, files{1})))));
    data = double(zeros(x,y,z,numFiles));
 
    for i = 1:numFiles
-       filePath = fullfile(directoryPath, files(i).name);
+       filePath = fullfile(path, files{i});
        dicom = squeeze(dicomread(filePath));
        data(:, :, :, i) = dicom;
    end
