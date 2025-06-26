3019df7e0e95743946ffd751d5b063b4d7d78902
harrisoncbrammell <harrisoncbrammell@gmail.com>
2025-06-25 19:34:32 -0500

    added normClamp
    normilizes an image between low and high then normilizes again between 0 and 1

---
+++ normClamp.m
@@ -0,0 +1,7 @@
+function normImg = normClamp(image,low,high) %%returns a normalized image clamped at low and high parameters
+    normImg = double(image);
+    normImg(normImg < low) = low;
+    normImg(normImg > high | isinf(normImg)) = high;
+    normImg = (normImg - low)/(high-low);
+    return;
+end
