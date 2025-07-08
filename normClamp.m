c675130868da6f60371259ce8c715e90db840689
harrisoncbrammell <harrisoncbrammell@gmail.com>
2025-07-08 13:45:38 -0500

    Update normClamp.m

--- normClamp.m
+++ normClamp.m
@@ -3,5 +3,5 @@ function normImg = normClamp(image,low,high) %%returns a normalized image clampe
     normImg(normImg < low) = low;
     normImg(normImg > high | isinf(normImg)) = high;
     normImg = (normImg - low)/(high-low);
-    return;
+   return;
 end
